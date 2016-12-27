75495c51fb3b6109bc56cb3b3215a1a9bc71133f
dzp307716494 <307716494@qq.com>
2016-12-27 09:25:52 +0800

    resume12.27
    个人简历

---
+++ gulpfile.js
@@ -0,0 +1,116 @@
+﻿var gulp=require('gulp');
+var webserver=require('gulp-webserver');//webserver服务器模块
+var url=require('url');
+var fs=require('fs');//fs=filesystem
+var sass=require('gulp-sass');
+var webpack=require('gulp-webpack');
+var named=require('vinyl-named');
+var uglify=require('gulp-uglify');
+var minifyCss=require('gulp-minify-css');
+//版本管理
+var rev=require('gulp-rev');
+var revCollector=require('gulp-rev-collector');
+var watch=require('gulp-watch');
+//队列模块
+var sequence=require('gulp-watch-sequence');
+
+
+
+gulp.task('copy-index',function(){
+	return gulp.src('./src/index.html').pipe(gulp.dest('./www'));
+})
+
+gulp.task('copy-img',function(){
+	return gulp.src('./src/images/*.*').pipe(gulp.dest('./www/images'));
+})
+
+gulp.task('webserver', function() {
+  gulp.src('./www')
+    .pipe(webserver({
+      livereload: true,
+      //directoryListing:true,
+      open:true,
+      middleware:function(req,res,next){
+      	//获取浏览器中的url，将url进行解析操作
+      	var urlObj = url.parse(req.url,true),
+      	method = req.method;
+      	//如果url里输出了/skill.php,/project.php或者是/work，
+      	//那么我们就可以查找到urlObj.pathname为/skill.php,/project.php,/work
+      	//然后我们就可以通过这个变化的url地址内容去判断并且返回相应的
+      	//skill.json/project.json/work.json等数据文件的内容
+      	switch(urlObj.pathname){
+      		case '/skill':
+      			// Content-Type可以指定返回的文件的格式类型
+      			res.setHeader('Content-Type','application/json');
+      			//需要通过fileSystem文件操作函数，去读取指定目录下的json文件，并将读取到的内容返回到浏览器端
+      			fs.readFile('./src/mock/skill.json','utf-8',function(err,data){
+      				res.end(data);
+      			});
+      		return;
+      		case '/project':
+      			res.setHeader('Content-Type','application/json');
+      			fs.readFile('./src/mock/project.json','utf-8',function(err,data){
+      				res.end(data);
+      			});
+      		return;
+      		case '/work':
+      			res.setHeader('Content-Type','application/json');
+      			fs.readFile('./src/mock/work.json','utf-8',function(err,data){
+      				res.end(data);
+      			});
+      		return;
+      	}
+      	next(); // next是实现的循环
+      } // end middleware
+
+    })); // end gulp
+});
+
+
+gulp.task('sass',function(){
+	return gulp.src('./src/styles/index.scss')
+	.pipe(sass()).pipe(minifyCss())
+	.pipe(gulp.dest('./www/css'));
+})
+
+gulp.task('packjs',function(){
+	return gulp.src('./src/scripts/index.js').pipe(named()).pipe(webpack()).pipe(uglify())
+	.pipe(gulp.dest('./www/js'));
+})
+
+var cssDistFiles=['./www/css/index.css'];
+var jsDistFiles=['./www/js/index.js'];
+
+gulp.task('verCss',function(){
+	return gulp.src(cssDistFiles).pipe(rev()).pipe(gulp.dest('./www/css'))
+	.pipe(rev.manifest()).pipe(gulp.dest('./www/ver/css'))
+})
+gulp.task('verJs',function(){
+	return gulp.src(jsDistFiles).pipe(rev()).pipe(gulp.dest('./www/js'))
+	.pipe(rev.manifest()).pipe(gulp.dest('./www/ver/js'))
+})
+
+gulp.task('html',function(){
+	gulp.src(['./www/ver/**/*.json','./www/*.html']).pipe(revCollector({
+		replaceReved:true
+	})).pipe(gulp.dest('./www'))
+})
+
+
+
+gulp.task('watch',function(){
+	gulp.watch('./src/index.html',['copy-index']);
+	gulp.watch('./src/images/*.*',['copy-img']);
+	var queue=sequence(300);
+	watch('./src/scripts/**/*.js',{
+		name:"JS",
+		emitOnGlob:false
+	},queue.getHandler('packjs','verJs','html'));
+	watch('./src/styles/**',{
+		name:"CSS",
+		emitOnGlob:false
+	},queue.getHandler('sass','verCss','html'));
+})
+
+gulp.task('default',['webserver','watch']);
+
---
+++ src/scripts/index.js
@@ -0,0 +1,110 @@
+var $ = require('./common/libs/zepto-modules/zepto');
+require('./common/libs/zepto-modules/event');
+require('./common/libs/zepto-modules/ajax');
+require('./common/libs/zepto-modules/touch');
+
+var Swiper = require('./common/libs/swiper/swiper.min.js');
+var swiperAni = require('./common/libs/swiper/swiper.animate1.0.2.min.js');
+var IScroll = require('./common/libs/iscroll/iscroll.js');
+
+// edit index
+$(".swiper-container").show();
+$("#mainContainer").hide();
+
+var swiper = new Swiper('.swiper-container',{
+  onInit: function(swiper){ //Swiper2.x的初始化是onFirstInit
+    swiperAni.swiperAnimateCache(swiper); //隐藏动画元素 
+    swiperAni.swiperAnimate(swiper); //初始化完成开始动画
+  }, 
+  onSlideChangeEnd: function(swiper){ 
+    swiperAni.swiperAnimate(swiper); //每个slide切换结束时也运行当前slide动画
+  }, 
+  pagination: '.swiper-pagination',
+  paginationClickable: true
+  
+});
+
+var myScroll;
+
+
+$("#enter").tap(function(){
+	$(".swiper-container").hide();
+	$("#mainContainer").show();
+	myScroll = new IScroll('#wrapper', { mouseWheel: true });
+	document.addEventListener('touchmove', function (e) { e.preventDefault(); }, false);
+})
+
+function scroll(){
+	myScroll = new IScroll('#scroll', { mouseWheel: true });
+	document.addEventListener('touchmove', function (e) { e.preventDefault(); }, false);
+}
+
+$('.footer-btn').tap(function(){
+	$('#scroller').hide();
+	$('#scroll').find('ul').html('');
+	$(this).addClass('onit').siblings('.footer-btn').removeClass('onit')
+	var sid=$(this).attr('id');
+	if(sid=='me'){	
+		$('#mainContainer').css('backgroundImage',"url(../images/blue.jpg)");
+		$('#scroller').show();
+		scroll();
+		myScroll.refresh();
+	}
+	
+	
+	if(sid=='skill'){
+		$('#mainContainer').css('backgroundImage',"url(../images/black.jpg)")
+		$.post('http://localhost:8000/'+sid,function(data){
+			var num=data.length,string='';
+			for(var i=0;i<num;i++){
+				string+='<li class="skill"><img src="./images/'+i+'.jpg"/><div><p>'+data[i].category+'</p><p>'+data[i].name+'</p><p>'+data[i].time+'</p><p>'+data[i].level+'</p></div></li>'
+			}
+			$('#scroll').find('ul').html(string);
+			scroll();
+			myScroll.refresh();
+		})
+	}
+	
+	
+	if(sid=='work'){
+		$('#mainContainer').css('backgroundImage',"url(../images/computer.jpg)")
+		$.post('http://localhost:8000/'+sid,function(data){
+			var num=data.length,string='';
+			for(var i=0;i<num;i++){
+				string+='<li class="work"><img src="./images/trig.svg"/><div><p>'+data[i].time+'</p><p>'+data[i].name+'</p></div></li>'	
+			}
+			$('#scroll').find('ul').html(string);
+			scroll();
+		})
+		
+	}
+	if(sid=='project'){
+		$('#mainContainer').css('backgroundImage',"url(../images/black.jpg)")
+		$.post('http://localhost:8000/'+sid,function(data){
+		var string='';
+			for(var i=0;i<2;i++){
+				string+='<div class="banner"><img src="./images/banner'+i+'.jpg" /></div>'
+				string+='<li class="project"><h3>'+data[i].name+'</h3><p>'+data[i].category+'</p><p>'+data[i].detail+'</p></li>'	
+			}
+			
+			$('#scroll').find('ul').html(string);
+			scroll();
+		})
+	}
+	
+	
+	
+	/*$.post('http://localhost:8000/project',function(data){
+		myScroll = new IScroll('#wrapper', { mouseWheel: true });
+		document.addEventListener('touchmove', function (e) { e.preventDefault(); }, false);
+			var num=data.length,string='';
+			for(var i=0;i<num;i++){
+				string+='<li>'+data[i].detail+'</li>'
+				string+='<li>'+data[i].description+'</li>'
+				string+='<li>'+data[i].detail+'</li>'
+				string+='<li>'+data[i].detail+'</li>'
+			}
+			$('#scroller').find('ul').html(string);
+		})*/
+	myScroll.refresh();
+})
